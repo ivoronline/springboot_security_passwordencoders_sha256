165b36d9397b25787644402906de1d14535250da
igoreta <ivoronline@gmail.com>
2021-02-15 14:55:54 +0100

    Initial commit

---
+++ src/main/java/com/ivoronlne/springboot_security_passwordencoders_sha256/controllers/MyController.java
@@ -0,0 +1,34 @@
+package com.ivoronlne.springboot_security_passwordencoders_sha256.controllers;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+  @ResponseBody
+  @RequestMapping("/EncodePassword")
+  public String encodePassword(@RequestParam String password) {
+
+    //GET PASSWORD ENCODER
+    PasswordEncoder passwordEncoder = new StandardPasswordEncoder();
+
+    //ENCODE PASSWORD
+    String encodedPassword = passwordEncoder.encode(password);
+
+    //RETURN ENCODED PASSWORD
+    return encodedPassword;
+
+  }
+
+  @ResponseBody
+  @RequestMapping("/Hello")
+  public String hello() {
+    return "Hello from Controller";
+  }
+
+}
---
+++ src/test/java/com/ivoronlne/springboot_security_passwordencoders_sha256/SpringbootSecurityPasswordencodersSha256ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronlne.springboot_security_passwordencoders_sha256;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityPasswordencodersSha256ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
